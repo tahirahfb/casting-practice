2f85e0edd483fb1d24e636f008b9d79070a0cf71
unknown <tahirahfb@gmail.com>
2022-05-04 13:23:44 -0400

    First Commit

---
+++ src/DoubleToInt.java
@@ -0,0 +1,9 @@
+public class DoubleToInt {
+    public static void main(String[] args) throws Exception {
+        double num1 = 112.35;
+        int num2 = (int)num1;
+
+        System.out.println("Double: " + num1);
+        System.out.println("int: " + num2);
+    }
+}
---
+++ src/StringToInt.java
@@ -0,0 +1,10 @@
+public class StringToInt {
+    public static void main(String[] args){
+        String num1 = "49";
+        int num2 = Integer.parseInt(num1);
+
+        System.out.println("The string value is: " + num1);
+        System.out.println("The integer value is: " + num2);
+    }
+    
+}
